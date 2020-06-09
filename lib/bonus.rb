c1b946299b8ed7802090ec063d2e23f9ab229ee7
Tyler Williams <tyler.williams2996@gmail.com>
2020-06-09 23:35:52 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -37,11 +37,11 @@ puts epic_tragedy
         {name: "Mercutio", age: "18", attitude: "hot-headed"}
       ]
    },
-   :capulet => {
-      :patriarch => {name: "Lord Capulet", age: "50"},
-      :matriarch => {name: "Lady Capulet", age: "51"},
-      :heroine => {name: "Juliet", age: "15", status: "alive"},
-      :heroine_friends => [
+   :capulet {
+      :patriarch{name: "Lord Capulet", age: "50"},
+      :matriarch  {name: "Lady Capulet", age: "51"},
+      :heroine {name: "Juliet", age: "15", status: "alive"},
+      :heroine_friends  [
         {name: "Steven", age: "30", attitude: "confused"},
         {name: "Nurse", age: "44", attitude: "worried"}
       ]
