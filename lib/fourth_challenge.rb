eb2ede7f3e19d43039c24292a693ee20a3077d07
Tyler Williams <tyler.williams2996@gmail.com>
2020-06-09 23:45:59 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -1,11 +1,12 @@
-epic_tragedy = {
+def fourth_challenge
+  epic_tragedy = {
    :montague => {
       :patriarch => {name: "Lord Montague", age: "53"},
       :matriarch => {name: "Lady Montague", age: "54"},
       :hero => {name: "Romeo", age: "15", status: "alive"},
       :hero_friends => [
-         {name: "Benvolio", age: "17", attitude: "worried"},
-         {name: "Mercutio", age: "18", attitude: "hot-headed"}
+        {:name => "Benvolio", :age => "17", :attitude => "worried"},
+        {:name => "Steven", :age => "30", :attitude => "confused"}
       ]
    },
    :capulet => {
@@ -13,8 +14,12 @@ epic_tragedy = {
       :matriarch => {name: "Lady Capulet", age: "51"},
       :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => [
-          {name: "Steven", age: "30", attitude: "confused"},
-          {name: "Nurse", age: "44", attitude: "worried"}
+          {:name => "Mercutio", :age => "18", :attitude => "hot-headed"},
+          {:name => "Nurse", :age => "44", :attitude => "worried"}
       ]
    }
 }
+
+
+
+end
